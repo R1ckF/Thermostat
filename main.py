eccac2b715341c84b32ced584111ecadc4395572
RickF <feith.rick@gmail.com>
2018-12-29 13:36:33 +0100

    website added

--- main.py
+++ main.py
@@ -1,86 +1,93 @@
+from flask import Flask, request
+
 import RPi.GPIO as GPIO
 from thermostatCommands import *
 import time
-from flask import Flask, request
-
+import logging
 topPin = 2
 bottomPin = 3
 topButton = 4
 bottomButton = 5
 
+LOGGER = logging.getLogger()
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+		
+
+	
+    # while True:
+    #    task = raw_input('Enter Up, Down or desired temperature: ')
+    #    if task == 'Up':
+    #        thermos.tempUp()
+    #    elif task == 'Down':
+    #        thermos.tempDown()
+    #    elif task.isdigit():
+    #        thermos.tempTarget = eval(task)
+    #    else:
+     #       print('Command not understood')
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/", methods=["GET", "POST"])
+def mainpage():
+	return '''
+		<html>
+			<body>
+				<p>Current temp: {temp}
+				<form method="post" action="/Up">
+					<p><input type="submit" value="Up" /></p>
+				</form>
+				<form method="post" action="/Down">
+					<p><input type="submit" value="Down" /></p>
+				</form>
+			</body>
+		</html>
+	'''.format(temp=thermos.temp)
+	
+@app.route("/Up", methods=["GET", "POST"])
+def Up():
+	thermos.tempUp()
+	return '''
+		<html>
+			<body>
+				<p>Current temp: {temp}
+				<form method="post" action="/Up">
+					<p><input type="submit" value="Up" /></p>
+				</form>
+				<form method="post" action="/Down">
+					<p><input type="submit" value="Down" /></p>
+				</form>
+			</body>
+		</html>
+	'''.format(temp=thermos.temp)
+	
+@app.route("/Down", methods=["GET", "POST"])
+def Down():
+	thermos.tempDown()
+	return '''
+		<html>
+			<body>
+				<p>Current temp: {temp}
+				<form method="post" action="/Up">
+					<p><input type="submit" value="Up" /></p>
+				</form>
+				<form method="post" action="/Down">
+					<p><input type="submit" value="Down" /></p>
+				</form>
+			</body>
+		</html>
+	'''.format(temp=thermos.temp)
+        
 try:
-    thermos = thermostat(topPin, bottomPin, topButton, bottomButton)
-	
-	
-	
-	while True:
-		task = input('Enter Up, Down or desired temperature: ')
-		if task == 'Up':
-			thermos.tempUp()
-		elif task == 'Down':
-			thermos.tempDown()
-		elif type(task) == int or type(task)==float:
-			thermos.setTemp(task)
-		else:
-			print('Command not understood')
-	
+    thermos = thermostat(topPin, bottomPin, topButton, bottomButton, LOGGER)
+    app.run(host='0.0.0.0',port=5000)
+    
 finally:
+    print('Error occurred, clean GPIOs')
     GPIO.cleanup()
 
 
-# app = Flask(__name__)
-	# app.config["DEBUG"] = True
-
-	# @app.route("/", methods=["GET", "POST"])
-	# def mainpage():
-		# return '''
-			# <html>
-				# <body>
-					# <p>Current temp: {temp}
-					# <form method="post" action="/Up">
-						# <p><input type="submit" value="Up" /></p>
-					# </form>
-					# <form method="post" action="/Down">
-						# <p><input type="submit" value="Down" /></p>
-					# </form>
-				# </body>
-			# </html>
-		# '''.format(temp=t.temp)
-		
-	# @app.route("/Up", methods=["GET", "POST"])
-	# def Up():
-		# thermos.up()
-		# return '''
-			# <html>
-				# <body>
-					# <p>Current temp: {temp}
-					# <form method="post" action="/Up">
-						# <p><input type="submit" value="Up" /></p>
-					# </form>
-					# <form method="post" action="/Down">
-						# <p><input type="submit" value="Down" /></p>
-					# </form>
-				# </body>
-			# </html>
-		# '''.format(temp=t.temp)
-		
-	# @app.route("/Down", methods=["GET", "POST"])
-	# def Down():
-		# thermos.down()
-		# return '''
-			# <html>
-				# <body>
-					# <p>Current temp: {temp}
-					# <form method="post" action="/Up">
-						# <p><input type="submit" value="Up" /></p>
-					# </form>
-					# <form method="post" action="/Down">
-						# <p><input type="submit" value="Down" /></p>
-					# </form>
-				# </body>
-			# </html>
-		# '''.format(temp=t.temp)
-		
+#
+                
 # class fakeThermos:
 	# def __init__(self, temp):
 		# self.temp=temp
--- thermostatCommands.py
+++ thermostatCommands.py
@@ -7,25 +7,25 @@ Control the set temp of thermostat
 Bottom leads top, temp up
 Top leads bottom, temp down
 5.0 is min
-30.0 is max
-USES BOARD NUMBERING!!
+35.0 is max
+USES BCM NUMBERING!!
 """
 
-class thermostat:
+class thermostat(object):
 
-    def __init__(self, topPin, bottomPin, topButton, bottomButton, temp=16.0, sleepTime=0.3, bt=100):
+    def __init__(self, topPin, bottomPin, topButton, bottomButton, LOGGER, temp=16.0, sleepTime=0.3, bt=100):
         self.topPin = topPin
         self.bottomPin = bottomPin
         self.topButton = topButton
         self.bottomButton  = bottomButton
-        self.tempTarget = temp
-        self.temp = 0
+        self.temp = 16
         self.sleepTime = sleepTime
-		self.min = 5.0
-		self.max = 30.0
-		
-        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-        GPIO.setmode(GPIO.BOARD)
+        self.min = 5.0
+        self.max = 35.0
+        self.bt = bt
+        self.LOGGER = LOGGER
+        self.tempTarget = temp
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.topPin, GPIO.OUT)
         GPIO.setup(self.bottomPin, GPIO.OUT)
         GPIO.setup(self.topButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -36,7 +36,7 @@ class thermostat:
         self.initialize()
 			
 	
-	@property
+    @property
     def tempTarget(self):
         return self.__tempTarget
 
@@ -44,74 +44,77 @@ class thermostat:
     def tempTarget(self, tempTarget):
         if tempTarget < self.min:
             self.__tempTarget = self.min
-			logging.info('Target temp to low, set to %s degrees instead', str(self.min))
+            logging.info('Target temp to low, set to %s degrees instead', str(self.min))
         elif tempTarget > self.max:
             self.__tempTarget = self.max
-			logging.info('Target temp to high, set to %s degrees instead', str(self.max))
+            logging.info('Target temp to high, set to %s degrees instead', str(self.max))
         else:
             self.__tempTarget = tempTarget
-		self.setTemp()
+            self.setTemp()
+        
 		
 	
 		
     def tempUp(self):
-		if self.temp < self.max:
-			if self.bottom == self.top:
-				self.changePin(self.bottomPin)
-			else:
-				self.changePin(self.topPin)
-			self.temp += 0.5
-			logging.info('Temperature increased to %s degrees', str(self.temp))
-			return True
-		else:
-			logging.info('Max temperature already reached')
-			return False
+        if self.temp < self.max:
+            if self.bottom == self.top:
+                self.changePin(self.bottomPin)
+            else:
+                self.changePin(self.topPin)
+            self.temp += 0.5
+            logging.info('Temperature increased to %s degrees', str(self.temp))
+            return True
+        else:
+            logging.info('Max temperature already reached')
+            return False
 
     def tempDown(self):
-		if self.temp > self.min
-			if self.top == self.bottom:
-				self.changePin(self.topPin)
-			else:
-				self.changePin(self.bottomPin)
-			self.temp -= 0.5
-			logging.info('Temperature lowered to %s degrees', str(self.temp))
-			return True
-		else:
-			logging.info('Minimum temperature already reached')
-			return False
+        if self.temp > self.min:
+            if self.top == self.bottom:
+                self.changePin(self.topPin)
+            else:
+                self.changePin(self.bottomPin)
+            self.temp -= 0.5
+            logging.info('Temperature lowered to %s degrees', str(self.temp))
+            return True
+        else:
+            logging.info('Minimum temperature already reached')
+            return False
     
     def setTemp(self):
         if self.temp > self.tempTarget:
             while self.temp > self.tempTarget:
                 self.tempDown()
-        if self.temp < self.tempTarget:
+        elif self.temp < self.tempTarget:
             while self.temp < self.tempTarget:
                 self.tempUp()
+        else:
+            logging.debug('Temperature already at correct level')
         
     def initialize(self):
         # first check buttons
         self.topButtonStateOld = self.topButtonState()
         self.bottomButtonStateOld = self.bottomButtonState()
 
-        logging.info('Turn thermostat 2 clicks to check buttons are working')
+        self.LOGGER.info('Turn thermostat 2 clicks to check buttons are working')
         startloop = time.time()
 
-        topButton, bottomButton = False, False
-        while time.time()< startloop + 30:
-            top, bottom = checkButtons()
-            if top:
+        topButton, bottomButton, success = False, False, False
+        while time.time()< startloop + 10:
+            top, bottom = self.checkButtons()
+            if not top:
                 topButton = True
-            if bottom:
+            if not bottom:
                 bottomButton = True
             if topButton and bottomButton:
-                succes = True
+                success = True
                 break
-        if succes:
-            logging.info("Buttons are recognized, adding triggers")
-			self.addTriggers()
+        if success:
+            self.LOGGER.info("Buttons are recognized, adding triggers")
+            self.addTriggers()
         else:
-            logging.warning("Buttons are not working")
-			raise TypeError
+            self.LOGGER.warning("Buttons are not working, Testmode is activated")
+            
 
         # secondly initialize temperature
         for i in range(40):
@@ -122,9 +125,9 @@ class thermostat:
         while self.temp < self.tempTarget:
             self.tempUp()
 
-        logging.info('Initialization complete, temperature set for %s degrees', str(self.temp))
+        self.LOGGER.info('Initialization complete, temperature set for %s degrees', str(self.temp))
 
-        return true
+        return True
 
     def controlButtons(self):
         if self.topButtonStateOld == self.bottomButtonStateOld:
@@ -144,15 +147,15 @@ class thermostat:
             
         
         #Functions for internal use
-	def addTriggers(self):
-		#adding triggers for buttons
-		GPIO.add_event_detect(self.topButton, GPIO.BOTH, callback=self.controlButtons, bouncetime=bt)
-		GPIO.add_event_detect(self.bottomButton, GPIO.BOTH, callback=self.controlButtons, bouncetime=bt)
+    def addTriggers(self):
+        #adding triggers for buttons
+        GPIO.add_event_detect(self.topButton, GPIO.BOTH, callback=self.controlButtons, bouncetime=self.bt)
+        GPIO.add_event_detect(self.bottomButton, GPIO.BOTH, callback=self.controlButtons, bouncetime=self.bt)
 		
-	def removeTriggers(self):
-		#remove triggers for buttons
-		GPIO.remove_event_detect(self.topButton)
-		GPIO.remove_event_detect(self.bottomButton)	
+    def removeTriggers(self):
+        #remove triggers for buttons
+        GPIO.remove_event_detect(self.topButton)
+        GPIO.remove_event_detect(self.bottomButton)
 	
     def checkButtons(self):
         # returns true when state is similar to previous state for top and bottom pin
@@ -206,13 +209,17 @@ class thermostat:
         if pin == self.bottomPin:
             if self.bottom == 1:
                 self.bottomOff()
+                self.bottom = 0
             else:
                 self.bottomOn()
+                self.bottom = 1
         else:
             if self.top == 1:
                 self.topOff()
+                self.top = 0
             else:
-                self.bottomOn()
+                self.topOn()
+                self.top = 1
         time.sleep(self.sleepTime)
         return True
 
