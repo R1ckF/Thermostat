ac04a6a404a1e74bc3edb9981d5020af813700a3
RickFeith <feith.rick@gmail.com>
2019-01-10 13:18:13 +0100

    added reset

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ bottomPin = 6
 topButton = 26
 bottomButton = 19
 
-awayTemp = 12
-homeTemp = 18
+OffTemp = 12
+OnTemp = 18
 startTemp = 15
 
 LOGGER = logging.getLogger()
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
-		
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG, filename='debug.log')
+
 app = Flask(__name__)
 app.config["DEBUG"] = False
 
@@ -31,16 +31,19 @@ def mainpage():
 				<form method="post" action="/Down">
 					<p><input type="submit" value="Down" /></p>
 				</form>
-				<form method="post" action="/Home">
-					<p><input type="submit" value="Home" /></p>
+				<form method="post" action="/On">
+					<p><input type="submit" value="On" /></p>
 				</form>
-				<form method="post" action="/Away">
-					<p><input type="submit" value="Away" /></p>
+				<form method="post" action="/Off">
+					<p><input type="submit" value="Off" /></p>
+				</form>
+				<form method="post" action="/Reset">
+					<p><input type="submit" value="Reset" /></p>
 				</form>
 			</body>
 		</html>
 	'''.format(temp=thermos.temp)
- 
+
 @app.route("/Up", methods=["GET", "POST"])
 def Up():
 	thermos.tempUp()
@@ -54,16 +57,19 @@ def Up():
 				<form method="post" action="/Down">
 					<p><input type="submit" value="Down" /></p>
 				</form>
-				<form method="post" action="/Home">
-					<p><input type="submit" value="Home" /></p>
+				<form method="post" action="/On">
+					<p><input type="submit" value="On" /></p>
 				</form>
-				<form method="post" action="/Away">
-					<p><input type="submit" value="Away" /></p>
+				<form method="post" action="/Off">
+					<p><input type="submit" value="Off" /></p>
+				</form>
+				<form method="post" action="/Reset">
+					<p><input type="submit" value="Reset" /></p>
 				</form>
 			</body>
 		</html>
 	'''.format(temp=thermos.temp)
- 
+
 @app.route("/Down", methods=["GET", "POST"])
 def Down():
 	thermos.tempDown()
@@ -77,19 +83,22 @@ def Down():
 				<form method="post" action="/Down">
 					<p><input type="submit" value="Down" /></p>
 				</form>
-				<form method="post" action="/Home">
-					<p><input type="submit" value="Home" /></p>
+				<form method="post" action="/On">
+					<p><input type="submit" value="On" /></p>
 				</form>
-				<form method="post" action="/Away">
-					<p><input type="submit" value="Away" /></p>
+				<form method="post" action="/Off">
+					<p><input type="submit" value="Off" /></p>
+				</form>
+				<form method="post" action="/Reset">
+					<p><input type="submit" value="Reset" /></p>
 				</form>
 			</body>
 		</html>
 	'''.format(temp=thermos.temp)
- 
-@app.route("/Home", methods=["GET", "POST"])
-def Home():
-	thermos.home()
+
+@app.route("/On", methods=["GET", "POST"])
+def On():
+	thermos.on()
 	return '''
 		<html>
 			<body>
@@ -100,19 +109,22 @@ def Home():
 				<form method="post" action="/Down">
 					<p><input type="submit" value="Down" /></p>
 				</form>
-				<form method="post" action="/Home">
-					<p><input type="submit" value="Home" /></p>
+				<form method="post" action="/On">
+					<p><input type="submit" value="On" /></p>
 				</form>
-				<form method="post" action="/Away">
-					<p><input type="submit" value="Away" /></p>
+				<form method="post" action="/Off">
+					<p><input type="submit" value="Off" /></p>
+				</form>
+				<form method="post" action="/Reset">
+					<p><input type="submit" value="Reset" /></p>
 				</form>
 			</body>
 		</html>
 	'''.format(temp=thermos.temp)
-	
-@app.route("/Away", methods=["GET", "POST"])
-def Away():
-	thermos.away()
+
+@app.route("/Off", methods=["GET", "POST"])
+def Off():
+	thermos.off()
 	return '''
 		<html>
 			<body>
@@ -123,29 +135,58 @@ def Away():
 				<form method="post" action="/Down">
 					<p><input type="submit" value="Down" /></p>
 				</form>
-				<form method="post" action="/Home">
-					<p><input type="submit" value="Home" /></p>
+				<form method="post" action="/On">
+					<p><input type="submit" value="On" /></p>
 				</form>
-				<form method="post" action="/Away">
-					<p><input type="submit" value="Away" /></p>
+				<form method="post" action="/Off">
+					<p><input type="submit" value="Off" /></p>
+				</form>
+				<form method="post" action="/Reset">
+					<p><input type="submit" value="Reset" /></p>
 				</form>
 			</body>
 		</html>
 	'''.format(temp=thermos.temp)
- 
- 
-        
+
+@app.route("/Reset", methods=["GET", "POST"])
+def Reset():
+	thermos.reset()
+	return '''
+		<html>
+			<body>
+				<p>Current temp: {temp}
+				<form method="post" action="/Up">
+					<p><input type="submit" value="Up" /></p>
+				</form>
+				<form method="post" action="/Down">
+					<p><input type="submit" value="Down" /></p>
+				</form>
+				<form method="post" action="/On">
+					<p><input type="submit" value="On" /></p>
+				</form>
+				<form method="post" action="/Off">
+					<p><input type="submit" value="Off" /></p>
+				</form>
+				<form method="post" action="/Reset">
+					<p><input type="submit" value="Reset" /></p>
+				</form>
+			</body>
+		</html>
+	'''.format(temp=thermos.temp)
+
+
+
 try:
-    thermos = thermostat(topPin, bottomPin, topButton, bottomButton, LOGGER, awayTemp = awayTemp, homeTemp = homeTemp, temp = startTemp)
+    thermos = thermostat(topPin, bottomPin, topButton, bottomButton, LOGGER, offTemp = OffTemp, onTemp = OnTemp, temp = startTemp)
     app.run(host='0.0.0.0',port=5000)
-    
+
 finally:
     print('Error occurred, clean GPIOs')
     GPIO.cleanup()
 
 
 #
-                
+
 # class fakeThermos:
 	# def __init__(self, temp):
 		# self.temp=temp
--- thermostatCommands.py
+++ thermostatCommands.py
@@ -13,32 +13,32 @@ USES BCM NUMBERING!!
 
 class thermostat(object):
 
-    def __init__(self, topPin, bottomPin, topButton, bottomButton, LOGGER, temp=15.0, sleepTime=0.1, bt=100, awayTemp=12, homeTemp=18):
+    def __init__(self, topPin, bottomPin, topButton, bottomButton, LOGGER, temp=15.0, sleepTime=0.1, bt=100, offTemp=14.5, onTemp=18):
         self.topPin = topPin
         self.bottomPin = bottomPin
         self.topButton = topButton
         self.bottomButton  = bottomButton
-        self.temp = 30
+        self.temp = None
         self.sleepTime = sleepTime
         self.min = 5.0
         self.max = 35.0
         self.bt = bt
-        self.awayTemp = awayTemp
-        self.homeTemp = homeTemp
+        self.offTemp = offTemp
+        self.onTemp = onTemp
         self.LOGGER = LOGGER
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.topPin, GPIO.OUT)
         GPIO.setup(self.bottomPin, GPIO.OUT)
         GPIO.setup(self.topButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.bottomButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        
+
         self.bottomOff()
         self.topOff()
         self.lastCommandTime = time.time()
         self.initialize(temp)
-        
 
-	
+
+
     @property
     def tempTarget(self):
         return self.__tempTarget
@@ -54,12 +54,12 @@ class thermostat(object):
         else:
             self.__tempTarget = tempTarget
             self.setTemp()
-        
-		
-	
-		
+
+
+
+
     def tempUp(self):
-        if time.time() - self.lastCommandTime > 3.5:
+        if (time.time() - self.lastCommandTime > 3.5) and (time.time() - self.lastCommandTime < 4.5):
             time.sleep(2)
             self.changePin(self.bottomPin)
             self.lastCommandTime=time.time()
@@ -78,7 +78,7 @@ class thermostat(object):
             return False
 
     def tempDown(self):
-        if time.time() - self.lastCommandTime > 3.5:
+        if (time.time() - self.lastCommandTime > 3.5) and (time.time() - self.lastCommandTime < 4.5):
             time.sleep(2)
             self.changePin(self.bottomPin)
             self.lastCommandTime=time.time()
@@ -95,13 +95,13 @@ class thermostat(object):
         else:
             logging.info('Minimum temperature already reached')
             return False
-    
-    def away(self):
-        self.tempTarget = self.awayTemp
-    
-    def home(self):
-        self.tempTarget = self.homeTemp
-    
+
+    def off(self):
+        self.tempTarget = self.offTemp
+
+    def on(self):
+        self.tempTarget = self.onTemp
+
     def setTemp(self):
         if self.temp > self.tempTarget:
             while self.temp > self.tempTarget:
@@ -111,7 +111,18 @@ class thermostat(object):
                 self.tempUp()
         else:
             logging.debug('Temperature already at correct level')
-        
+
+    def reset(self):
+        saveTemp = self.temp
+        self.LOGGER.info('Resetting thermostat to avoid drift')
+        for i in range(80):
+            self.tempDown
+
+        self.temp = self.min
+        self.tempTarget = saveTemp
+        self.LOGGER.info('Reset complete, temperature set for %s degrees', str(self.temp))
+
+
     def initialize(self, temp):
         # first check buttons
         self.topButtonStateOld = self.topButtonState()
@@ -135,7 +146,7 @@ class thermostat(object):
             self.addTriggers()
         else:
             self.LOGGER.warning("Buttons are not working, Testmode is activated")
-            
+
 
         # secondly initialize temperature
         for i in range(80):
@@ -162,21 +173,21 @@ class thermostat(object):
                 self.tempUp()
             elif not bottom:
                 self.tempDown()
-        
-        
-            
-        
+
+
+
+
         #Functions for internal use
     def addTriggers(self):
         #adding triggers for buttons
         GPIO.add_event_detect(self.topButton, GPIO.BOTH, callback=self.controlButtons, bouncetime=self.bt)
         GPIO.add_event_detect(self.bottomButton, GPIO.BOTH, callback=self.controlButtons, bouncetime=self.bt)
-		
+
     def removeTriggers(self):
         #remove triggers for buttons
         GPIO.remove_event_detect(self.topButton)
         GPIO.remove_event_detect(self.bottomButton)
-	
+
     def checkButtons(self):
         # returns true when state is similar to previous state for top and bottom pin
         if self.topButtonStateOld == self.topButtonState():
@@ -190,7 +201,7 @@ class thermostat(object):
             bottom = False
             self.bottomButtonStateOld = self.bottomButtonState()
         return top, bottom
-    
+
     def topButtonState(self):
         out =  GPIO.input(self.topButton)
         logging.debug('topButtonState: '+  str(out))
@@ -242,9 +253,3 @@ class thermostat(object):
                 self.top = 1
         time.sleep(self.sleepTime)
         return True
-
-    
-
-
-
-
